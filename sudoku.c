072627e0a35b14b6b528878245d648f81084030e
Your Name <you@example.com>
2025-05-14 08:38:04 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -48,36 +48,36 @@ int is_valid(Node* n){
 
     //filas
     for (i = 0; i < 9; i++){
-       int seen[10] = {0};
+       int visto[10] = {0};
        for (j = 0; j < 9; j++){
         int val = n->sudo[i][j];
         if (val != 0){
-            if (seen[val]) return 0;
-            seen[val] = 1;
+            if (visto[val]) return 0;
+            visto[val] = 1;
         }
        }
     }
     //columnas
     for (j = 0; j < 9; j++){
-      int seen[10] = {0};
+      int visto[10] = {0};
       for (i = 0; i < 9; i++){
         int val = n->sudo[i][j];
         if (val != 0){
-            if (seen[val]) return 0;
-            seen[val] = 1;
+            if (visto[val]) return 0;
+            visto[val] = 1;
         }
       }
     }
     //submatrices 3x3
     for (k = 0; k < 9 ; k++){
-      int seen[10] = {0};
+      int visto[10] = {0};
       for (int p = 0; p < 9; p++){
         int i = (k/3)*3 + (p/3);
         int j = (k%3)*3 + (p%3);
         int val = n->sudo[i][j];
         if (val != 0){
-            if (seen[val]) return 0;
-            seen[val] = 1;
+            if (visto[val]) return 0;
+            visto[val] = 1;
         }
       }
     }
